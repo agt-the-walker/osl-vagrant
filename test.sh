agt-the-walker/osl-vagrant

commit ceb93486997327f80f813cbd3d21378a1ebd69b7

Fix ShellCheck issues

test.sh
@@ -6,6 +6,6 @@ set -e
 
 cd osl
 
-[[ -h data ]] || ln -s ../gpsshogi/data
-[[ -h public-data ]] || ln -s ../data public-data
+[[ -h data ]] || ln -s ../gpsshogi/data .
+[[ -h public-data ]] || ln -s ../data public-data .
 make run-test

setup.sh
@@ -10,7 +10,7 @@ function Install {
         apt-get update
         has_update_run=1
     fi
-    apt-get install -y $package
+    apt-get install -y "$package"
 }
 
 ### Check environment
@@ -51,6 +51,6 @@ fi
 
 for dir in */; do
     if [[ -O $dir ]]; then
-        chown -R vagrant $dir
+        chown -R vagrant "$dir"
     fi
 done

commit 6838b0b09c001fe48ae15e646fceabe7d53e07fa

Fix bug introduced in ceb93486997327f80f813cbd3d21378a1ebd69b7

test.sh
@@ -7,5 +7,5 @@ set -e
 cd osl
 
 [[ -h data ]] || ln -s ../gpsshogi/data .
-[[ -h public-data ]] || ln -s ../data public-data .
+[[ -h public-data ]] || ln -s ../data public-data
 make run-test